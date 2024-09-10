9089db73d86924cb089bfd457594133a208fda6d
stevrodz <rodriguezsl82@yahoo.com>
2024-09-10 12:34:41 -0500

    added JavaScript

---
+++ index.js
@@ -0,0 +1,16 @@
+//Pseudocode: I will use JavaScript to create three variables. Each variable will be different in type but similar in action.
+
+const greeting =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log(greeting);
+
+var ten = 3 + 7;
+let forty = 5 * 8;
+const thirtyNine = 50 - 11;
+
+/*variables have been assigned.
+each contain the corresponding result of calculation using a unique arithmetic operator.*/
+
+alert("The combination of the lock is " + ten + "-" + forty + "-" + thirtyNine);
+
+//created pop up dialog using alert.
